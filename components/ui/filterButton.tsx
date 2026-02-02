c30eaa6aee113aa725a366e82332c41f9983c434
lancememan <lancememan1994@gmail.com>
2026-02-03 04:35:00 +0800

    isOpen to type boolean; updated baseline browser mapping to latest

--- components/ui/filterButton.tsx
+++ components/ui/filterButton.tsx
@@ -1,8 +1,12 @@
 "use client";
 import { FaFilter } from "react-icons/fa";
 
+type DealsFilterProps = {
+  isOpen: boolean;
+  updateState: (state: boolean) => void;
+} 
 
-const DealsFilter = ({isOpen, updateState}) => {
+const DealsFilter = ({isOpen, updateState}: DealsFilterProps) => {
   
     return (
       <div className="bg-gray-800 fixed bottom-10 right-10 p-2.5 rounded-md cursor-pointer hover:bg-gray-800/80 z-10" onClick={() => updateState(!isOpen)}>
